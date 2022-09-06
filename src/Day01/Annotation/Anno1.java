00d77169bb5e6a75ae95f59e5ed22463f7f6a6f8
dh <1024057801@qq.com>
2022-09-06 18:07:13 +0800

    CommitDay01_AnnotationPractice

---
+++ src/Day01/Annotation/Anno1.java
@@ -0,0 +1,20 @@
+package Day01.Annotation;
+
+/**
+ * 注解javadoc练习
+ * @author dn
+ * @version 1.0
+ * @since 1.5
+
+ */
+public class Anno1 {
+    /**
+     *计算两数的和
+     * @param a int
+     * @param b int
+     * @return a + b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
